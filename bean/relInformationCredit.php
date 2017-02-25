ce1d72deac8c853a226ef3947128aa5e1591c35c
ahmedjorge <ahmedferreira11@gmail.com>
2017-02-25 18:55:12 +0000

    ahmed commit

---
+++ bean/relInformationCredit.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmedjorge
+ * Date: 25-02-2017
+ * Time: 16:27
+ */
+include "../conexao/CallPgSQL.php";
+include "../modelo/User.php";
+include "Session.php";
+
+if($_POST["intensao"] == "loadListCredits"){ loadCredit(); }
+if($_POST["intensao"] == "loadListInformationCredit"){ loadInformationByCredit(); }
+
+function loadCredit(){
+    $call = new CallPgSQL();
+    $call->functionTable("report.funct_rep_credits_filter","*")
+        ->addString(Session::getUserLogado()->getId())
+        ->addJsonb(null);
+    $call->execute();
+    $credits = array();
+    while ($value = $call->getValors()){
+        $credits[count($credits)] = $value;
+    }
+    die(json_encode(array("credits" => $credits)));
+}
+
+function loadInformationByCredit(){
+    $call = new CallPgSQL();
+    $call->functionTable("report.funct_rep_credits_garrantia","*")
+        ->addNumeric(Session::getUserLogado()->getId())
+        ->addNumeric($_POST["id"])
+        ->addJsonb(null);
+    $call->execute();
+    $information = array();
+    while ($value = $call->getValors()){
+        $value["type"] = "Garrantia";
+        $information[count($information)] = $value;
+    }
+
+    $call->functionTable("report.funct_rep_credits_garrantia","*")
+        ->addNumeric(Session::getUserLogado()->getId())
+        ->addNumeric($_POST["id"])
+        ->addJsonb(null);
+    $call->execute();
+    while ($value = $call->getValors()){
+        $value["type"] = "Documento";
+        $information[count($information)] = $value;
+    }
+    die(json_encode(array("information" => $information)));
+}
--- resources/js/reports/warranty.js
+++ resources/js/reports/warranty.js
@@ -1,26 +1,28 @@
+loadDataToPage();
+
 $('.list-warr').on("click",".first small", function(event) {
 	me = $(this).closest('section');
 	me.find('.more').toggleClass('show');
     var ig = Number($(this).attr("id-id"));
+
 	if(!me.hasClass("show")) {
-        garantiaBluider.getListGrantiaDescription(ig);
-    }else{
-        $("#"+ig).empty();
-    }
+        if(creditBluider.listaCredito[ig].listaInformacao.length == 0 ){ getInformationCredit(ig, creditBluider.listaCredito[ig]); }
+        else{ creditBluider.getListInformationCredit(ig); }
+    }else{ $("#"+ig).empty(); }
 });
 
-var GarantiaClienteBluider = function () {
+var CreditoBluider = function () {
     /**
-     * @type {[GarantiaClient]}
+     * @type {[Credito]}
      */
-    this.listaGarantiaCliente = [];
-    this.getGarrantiaCliente = function () {
+    this.listaCredito = [];
+    this.getCreditos = function () {
         var rt = "";
-        for (var ig = 0; ig < this.listaGarantiaCliente.length; ig++){
+        for (var ig = 0; ig < this.listaCredito.length; ig++){
             rt += '<section>' +
                     '<div class="first">' +
-                        '<b><span>Crédito nº <span>'+this.listaGarantiaCliente[ig].numCredito+'</span></span><small id-id="'+ig+'">Mais detalhes</small></b>' +
-                        '<h3>'+this.listaGarantiaCliente[ig].clienteNome+'</h3>' +
+                        '<b><span>Crédito nº <span>'+this.listaCredito[ig].number+'</span></span><small id-id="'+ig+'">Mais detalhes</small></b>' +
+                        '<h3>'+this.listaCredito[ig].clienteNome+'</h3>' +
                     '</div>' +
                     '<div class="more" id="'+ig+'" ></div>' +
                 '</section>';
@@ -31,31 +33,40 @@ var GarantiaClienteBluider = function () {
     /**
      * @param iClient {Number}
      */
-    this.getListGrantiaDescription = function (iClient) {
+    this.getListInformationCredit = function (iClient) {
         var rt = "";
         /**
-         * @type {GarantiaClient}
+         * @type {Credito}
          */
-        var garantiaCliente = this.listaGarantiaCliente[iClient];
-        for (var ig = 0; ig < garantiaCliente.listaInformacao.length; ig++ ){
-            var garantia = garantiaCliente.listaInformacao[ig];
+        var Creditoe = this.listaCredito[iClient];
+        for (var ig = 0; ig < Creditoe.listaInformacao.length; ig++ ){
+            var infor = Creditoe.listaInformacao[ig];
             rt +=
-                '<h4>'+garantia.tipo+'</h4>' +
-                '<p>'+garantia.descricao+'</p>';
+                '<h4>'+infor.tipo+' - '+infor.name+'</h4>' +
+                '<p>'+infor.value+'</p>';
         }
         $("#"+iClient).html(rt);
     };
 
     /**
-     * @param GarClient {GarantiaClient}
+     * @param GarClient {Credito}
      */
-    this.addClienteGarantia = function (GarClient) {
-        this.listaGarantiaCliente[this.listaGarantiaCliente.length] = GarClient;
+    this.addCredito = function (GarClient) {
+        this.listaCredito[this.listaCredito.length] = GarClient;
     }
 };
 
-var GarantiaClient = function () {
-    this.numCredito = undefined;
+var Credito = function () {
+    this.cheque = undefined;
+    this.id = undefined;
+    this.name = undefined;
+    this.nif = undefined;
+    this.number = undefined;
+    this.other = undefined;
+    this.state = undefined;
+    this.surname = undefined;
+    this.totalpagar = undefined;
+    this.value = undefined;
     this.clienteNome = undefined;
     /**
      *
@@ -67,36 +78,76 @@ var GarantiaClient = function () {
      *
      * @param information {Information}
      */
-    this.addGarantia = function (information) {
+    this.addInformation = function (information) {
         this.listaInformacao[this.listaInformacao.length] = information;
     }
 };
 
 var Information = function () {
+    this.id = undefined;
     this.tipo = undefined;
-    this.idTipo = undefined;
-    this.descricao = undefined;
+    this.name = undefined;
+    this.value = undefined;
 };
 
-var garantiaBluider = new GarantiaClienteBluider();
+var creditBluider = new CreditoBluider();
 function loadDataToPage() {
-    for (var i = 0; i < 10; i++ ){
-        var garantiaCliente = new GarantiaClient();
-        garantiaCliente.clienteNome = "fjjfjf-"+i;
-        garantiaCliente.numCredito = "554545---"+i;
-
-        for (var j = 0; j < 7; j++){
-            var information = new Information();
-            information.tipo = i+"-tipe-"+j;
-            information.descricao = i+"-fhfhfhf-"+j;
-            information.idTipo = j;
-            garantiaCliente.addGarantia(information);
-
-        }
-        garantiaBluider.addClienteGarantia(garantiaCliente);
-    }
-
-    $(".list-warr").html(garantiaBluider.getGarrantiaCliente());
+    $.ajax({
+        url: "../../bean/relInformationCredit.php",
+        type: "POST",
+        data: {intensao: "loadListCredits", filter: null},
+        dataType: "json",
+        success: function (e) {
+            for (var f=0; (f < e.credits.length && f != 199) ; f++){
+                var Creditoe = new Credito();
+                var credits = e.credits[f];
+                Creditoe.cheque = credits["cheque"];
+                Creditoe.id = credits["id"];
+                Creditoe.name = credits["name"];
+                Creditoe.nif = credits["nif"];
+                Creditoe.number = credits["number"];
+                Creditoe.other = credits["other"];
+                Creditoe.state = credits["state"];
+                Creditoe.surname = credits["surname"];
+                Creditoe.totalpagar = credits["totalpagar"];
+                Creditoe.value = credits["value"];
+                Creditoe.clienteNome = (Creditoe.name+" "+((Creditoe.surname == Creditoe.name)
+                    ? "" : Creditoe.surname ));
+                creditBluider.addCredito(Creditoe);
+            }
+            $(".list-warr").html(creditBluider.getCreditos());
+        }, beforeSend: function () {  $(".mp-loading").fadeIn(); },
+        complete: function () { $(".mp-loading").fadeOut();}
+    });
 }
 
-loadDataToPage();
+/**
+ * @param i {Number}
+ * @param credit {Credito}
+ */
+function getInformationCredit(i,credit) {
+    $.ajax({
+        url: "../../bean/relInformationCredit.php",
+        type: "POST",
+        data: {intensao: "loadListInformationCredit", id: credit.id},
+        dataType: "json",
+        success: function (e) {
+            for (var f=0; (f < e.information.length && f != 199) ; f++){
+                var info = e.information[f];
+                var newInfo = new Information();
+                newInfo.id = info["ID"];
+                newInfo.tipo = info["tipo"];
+                if (newInfo.tipo == "Garrantia") {
+                    newInfo.name = info["GARRANTIA"];
+                    newInfo.value = info["GARRANTIAVALUE"];
+                } else {
+                    newInfo.name = info["DOCUMENT"];
+                    newInfo.value = info["DOCUMENTVALUE"];
+                }
+                credit.addInformation(newInfo);
+            }
+            creditBluider.getListInformationCredit(i);
+        }, beforeSend: function () {  $(".mp-loading").fadeIn(); },
+        complete: function () { $(".mp-loading").fadeOut();}
+    });
+}
