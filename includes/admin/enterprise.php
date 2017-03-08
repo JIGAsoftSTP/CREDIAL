3ac362a3aee73f9100b2e6f6e71cf88a8ec7f280
personal-xpert <juniorant_1995@hotmail.com>
2017-03-08 17:07:46 +0000

    sdb

--- includes/admin/enterprise.php
+++ includes/admin/enterprise.php
@@ -5,6 +5,7 @@
 	<script type="text/javascript" src="../../resources/js/admission.js"></script>
 	<link rel="stylesheet" href="../../resources/css/geralStyle.css">
 	<link rel="stylesheet" href="../../resources/css/admin.css">
+	<link rel="stylesheet" href="../../resources/css/admin/enterprise.css">
 
 </head>
 <body>
@@ -13,13 +14,21 @@
 			<h1 class="title">Empresa</h1>
 		</nav>
 		<div class="second">
-			<div class="lft"></div>
-			<div class="rght xpert-form flex-form">
-				<input type="text" placeholder="Nome" class="input-total" id="">
-				<input type="text" placeholder="NIF" class="input-total" id="">
-				<input type="text" placeholder="Telefone" class="input-total" id="">
-				<input type="text" placeholder="Email" class="input-total" id="">
-			</div>
+		<div class="content">
+		<input type="file" id="logo-ipt">
+			<nav class="logo">
+				<label for="logo-ipt" class="icon-image"></label>
+			</nav>
+			<section>
+				<div class="rght xpert-form flex-form">
+					<input type="text" placeholder="Nome" class="input-total" id="">
+					<input type="text" placeholder="NIF" class="input-total" id="">
+					<input type="text" placeholder="Telefone" class="input-total" id="">
+					<input type="text" placeholder="Email" class="input-total" id="">
+				</div>
+			<button>Concluir</button>
+			</section>
+		</div>
 		</div>
 		
 	</div>
