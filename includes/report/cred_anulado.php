6bea5defa659c84734dc16a1d7ad0935e5c146f7
ahmedjorge <ahmedferreira11@gmail.com>
2017-04-26 19:41:04 +0000

    siie-3 commit

---
+++ includes/report/cred_anulado.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="UTF-8">
+	<script type="text/javascript" src="../../resources/js/admission.js"></script>
+	<link rel="stylesheet" href="../../resources/css/geralStyle.css">
+	<link rel="stylesheet" href="../../resources/css/report.css">
+	<link rel="stylesheet" href="../../resources/css/reports/report-all.css">
+	
+	
+</head>
+<body>
+	<div class="master-content">
+
+		<div class="table-list">
+			<nav class="to-report">
+				<h2></h2>
+			</nav>
+			
+
+
+			<div class="x-table table-client auto-width">
+				<table id="table-report-credit" class="" cellpadding="0" cellspacing="0">
+					<thead>
+						<tr>
+							<th grow="2" orderBy>Nº Dossier</th>
+							<th grow="1" orderBy>Data Registo</th>
+							<th grow="1" orderBy>NIF</th>
+							<th grow="4" orderBy>Cliente</th>
+							<th grow="2" orderBy>Valor Crédito</th>
+							<th grow="2" orderBy>Montante Dívida</th>
+							<th grow="1" orderBy>TAEG</th>
+							<th grow="1">Data Inicio</th>
+							<th grow="1">Data Fim</th>
+						</tr>
+					</thead>
+					<tbody id="table-report-credit-body">
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+    <?php include "../process.html"; ?>
+</body>
+<script type="text/javascript" src="../../resources/js/jQuery.js"></script>
+<script type="text/javascript" src="../../resources/js/table.js"></script>
+<script type="text/javascript" src="../../resources/js/geralScript.js"></script>
+</html>
+
+
