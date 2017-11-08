86a6863e3ca05347032e4b48e677a4cc59922466
ahmedjorge <ahmedferreira11@gmail.com>
2017-11-08 12:24:17 +0000

    Commit new

---
+++ includes/report/pag_antecipado.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="UTF-8">
+	<script type="text/javascript" src="../../resources/js/admission.js"></script>
+	<link rel="stylesheet" href="../../resources/css/geralStyle.css">
+	<link rel="stylesheet" href="../../resources/css/report.css">
+	<link rel="stylesheet" href="../../resources/css/reports/report-all.css">
+	
+	
+</head>
+<body>
+	<div class="master-content">
+
+		<div class="table-list">
+			<nav class="to-report">
+				<h2></h2>
+			</nav>
+			
+
+
+			<div class="x-table table-client auto-width">
+				<table id="table-client" class="" cellpadding="0" cellspacing="0">
+					<thead>
+						<tr>                    
+							<th grow="1.5">NIF</th>
+							<th grow="5">Nome</th>
+							<th grow="1">Crédito</th>
+							<th grow="3">Total pedido</th>
+						</tr>
+					</thead>
+					<tbody id="tableCliente">
+						<tr><td>109935310</td><td>WALDEMIRO QUARESMA FERNANDES</td><td>5</td><td>5 000 000</td></tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+    <?php include "../process.html"; ?>
+</body>
+<script type="text/javascript" src="../../resources/js/jQuery.js"></script>
+<script type="text/javascript" src="../../resources/js/table.js"></script>
+<script type="text/javascript" src="../../resources/js/geralScript.js"></script>
+</html>
+
+
