948d81afbbf98ad3a93a837cec8ebd6189d2b404
ahmedjorge <ahmedferreira11@gmail.com>
2017-12-08 11:53:28 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	resources/json/log-mail-send.json
    #	resources/json/save-log-mail-send.json

---
+++ includes/report/notif_credito.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="../../resources/js/admission.js"></script>
+    <link rel="stylesheet" href="../../resources/css/geralStyle.css">
+    <link rel="stylesheet" href="../../resources/css/report.css">
+    <link rel="styleshteet" href="../../resources/css/reports/report-all.css">
+    <link rel="stylesheet" href="../../resources/css/activity.css">
+    <link rel="stylesheet" href="../../resources/css/reports/warranty.css">
+</head>
+<body class="bd-warr">
+
+<div class="arend">
+    <div class="list-warr">
+    </div>
+</div>
+
+<?php include "../process.html"; ?>
+<div  id="labelAnulado" >
+</div>
+
+</body>
+<script type="text/javascript" src="../../resources/js/jQuery.js"></script>
+<script type="text/javascript" src="../../resources/js/geralScript.js"></script>
+<script type="text/javascript" src="../../resources/js/reports/report.js"></script>
+<script type="text/javascript" src="../../resources/js/reports/anulados.js"></script>
+</html>
